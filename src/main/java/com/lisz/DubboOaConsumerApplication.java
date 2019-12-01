64a8d2d9f07dde34448a1f19cd1e1e1c4527f006
lisz1012 <lisz1012@163.com>
2019-11-30 18:49:53 -0800

    Add notes

--- src/main/java/com/lisz/DubboOaConsumerApplication.java
+++ src/main/java/com/lisz/DubboOaConsumerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})//这里为了不让springboot去找data source的url
 public class DubboOaConsumerApplication {
 
 	public static void main(String[] args) {
