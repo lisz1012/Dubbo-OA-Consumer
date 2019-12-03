ed538fbb510860955f5504fbe8fa701acc4f930c
lisz1012 <lisz1012@163.com>
2019-12-02 23:49:51 -0800

    Add notes

--- src/main/java/com/lisz/controller/WeChatController.java
+++ src/main/java/com/lisz/controller/WeChatController.java
@@ -1,8 +1,14 @@
 package com.lisz.controller;
 
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,7 +17,8 @@ public class WeChatController {
 	
 	@GetMapping("signature")
 	@ResponseBody
-	public String signature() {
+	public String signature(@RequestParam Map<String, String> params, HttpServletRequest request, HttpServletResponse response) {
+		// TODO加入验证
 		return "Hello, WeChat";
 	}
 }
